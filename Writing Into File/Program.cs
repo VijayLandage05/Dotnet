6414c76e1a20ef76fbedf35e873a8d8d82a1c9a3
Vijay Landage <119501139+VijayLandage05@users.noreply.github.com>
2023-02-05 23:59:21 +0530

    Add files via upload

---
+++ Writing Into File/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace WriteFile
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\Vijay Landage\OneDrive\Desktop\Module End Test -OOPs with Java-2_files\input.txt";
+            string lines = "Welcome to the file location";
+
+            if(File.Exists(filepath))
+            {
+                File.WriteAllText(filepath, lines);
+                Console.WriteLine("Writing into file completed");                
+            }else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+    }
+}
